bd71aae8494d40c13ff1544c531c860376d43072
Badwoman <3242204470@qq.com>
2024-11-17 21:37:57 +0800

    finish lab8

--- kernel/sysfile.c
+++ kernel/sysfile.c
@@ -503,3 +503,34 @@ sys_pipe(void)
   }
   return 0;
 }
+
+uint64 sys_symlink(void){
+  char target[MAXPATH], path[MAXPATH];
+  struct inode *ip;
+
+  if(argstr(0, target, MAXPATH) < 0 || argstr(1, path, MAXPATH) < 0)
+    return -1;
+
+  begin_op();
+  
+  if((ip = namei(path)) != 0){
+    end_op();
+    return -1;
+  }
+  
+  ip = create(path, T_SYMLINK, 0, 0);
+  if(ip == 0){
+    end_op();
+    return -1;
+  }
+
+  if(writei(ip, 0, (uint64)target, 0, MAXPATH) < 0){
+    iunlockput(ip);
+    end_op();
+    return -1;
+  }
+  
+  iunlockput(ip);
+  end_op();
+  return 0;
+}
